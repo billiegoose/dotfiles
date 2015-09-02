1d8f4a60bd8dbf95c6aebaebb8fc7b6de36afee1
William Hilton <wmhilton@gmail.com>
2015-09-02 15:25:55 -0400

    OK, pretty much works now. :)

--- bin/.setup/install_basics.sh
+++ bin/.setup/install_basics.sh
@@ -2,8 +2,9 @@
 cd ~/bin
 # Install ack
 unlink ack
-curl http://beyondgrep.com/ack-2.14-single-file > ~/bin/ack && chmod 0755 !#:3
+curl http://beyondgrep.com/ack-2.14-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
 # Install text-adept
 unlink ta
 curl http://foicica.com/textadept/download/textadept_LATEST.x86_64.tgz | tar xz
 ln -s ./textadept_*/textadeptjit-curses ta
+echo "PLEASE LOG OUT AND LOG IN AGAIN TO USE INSTALLED PROGRAMS IN ~/bin"
