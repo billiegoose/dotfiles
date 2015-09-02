1b04a428fba50780b13102026c5daad8c06bb49d
William Hilton <wmhilton@gmail.com>
2015-09-02 14:59:32 -0400

    Start of magic install_basics.sh script

---
+++ bin/install_basics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~/bin
+# Install ack
+unlink ack
+curl http://beyondgrep.com/ack-2.14-single-file > ~/bin/ack && chmod 0755 !#:3
+# Install text-adept
+unlink ta
+curl http://foicica.com/textadept/download/textadept_LATEST.x86_64.tgz | tar xvz
+ln -s ./textadept_*/textadeptjit-curses ta
